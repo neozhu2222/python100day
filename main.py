ef77a3fbed9a07e252c7ab67629b24324ed6d6da
neozhu2222 <109549221+neozhu2222@users.noreply.github.com>
2022-07-27 18:26:13 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,59 +1,23 @@
-from datetime import datetime, timedelta
-from data_manager import DataManager
-from flight_search import FlightSearch
-from notification_manager import NotificationManager
+from flask import Flask, render_template
+import random
+import datetime
+import requests
+
+app = Flask(__name__)
+
+@app.route('/guess/<name>')
+def guess(name):
+    gender_url = f"url{name}"
+    gender_response = requests.get(gender_url)
+    gender_data = gender_response.json()
+    gender = gender_data["gender"]
+    age_url = f"url{name}"
+    age_response = requests.get(age_url)
+    age_data = age_response.json()
+    age = age_data["age"]
+    return render_template("guess.html", person_name=name)
 
 
-ORIGIN_CITY_IATA = "LON"
 
-data_manager = DataManager()
-flight_search = FlightSearch()
-notification_manager = NotificationManager()
-
-sheet_data = data_manager.get_destination_data()
-
-if sheet_data[0]["iataCode"] == "":
-    city_names = [row["city"] for row in sheet_data]
-    data_manager.city_codes = flight_search.get_destination_codes(city_names)
-    data_manager.update_destination_codes()
-    sheet_data = data_manager.get_destination_data()
-
-destinations = {
-    data["iataCode"]: {
-        "id": data["id"],
-        "city": data["city"],
-        "price": data["lowestPrice"]
-    } for data in sheet_data}
-
-tomorrow = datetime.now() + timedelta(days=1)
-six_month_from_today = datetime.now() + timedelta(days=6 * 30)
-
-for destination_code in destinations:
-    flight = flight_search.check_flights(
-        ORIGIN_CITY_IATA,
-        destination_code,
-        from_time=tomorrow,
-        to_time=six_month_from_today
-    )
-    if flight is None:
-        continue
-
-    if flight.price < destinations[destination_code]["price"]:
-
-        users = data_manager.get_customer_emails()
-        emails = [row["email"] for row in users]
-        names = [row["firstName"] for row in users]
-
-        message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
-        if flight.stop_overs > 0:
-            message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
-
-        link = f"https://www.google.co.uk/flights?hl=en#flt={flight.origin_airport}.{flight.destination_airport}.{flight.out_date}*{flight.destination_airport}.{flight.origin_airport}.{flight.return_date}"
-
-        ######################
-        if flight.stop_overs > 0:
-            message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
-            print(message)
-        #######################
-
-        notification_manager.send_sms(message)
+if __name__ == "__main__":
+    app.run(debug=True)
