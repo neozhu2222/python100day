52879599cf0c74fd2763cf6d0f8d50c28355a2e7
neozhu2222 <109549221+neozhu2222@users.noreply.github.com>
2022-08-03 20:58:07 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,103 +1,54 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-import requests
+import csv
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
-##CREATE DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+
+class CafeForm(FlaskForm):
+    cafe = StringField('Cafe name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+# Exercise:
+# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
+# make coffee/wifi/power a select element with choice of 0 to 5.
+#e.g. You could use emojis ☕️/💪/✘/🔌
+# make all fields required except submit
+# use a validator to check that the URL field has a URL entered.
+# ---------------------------------------------------------------------------
 
 
-##CREATE TABLE
-class Movie(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    year = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String(500), nullable=False)
-    rating = db.Column(db.Float, nullable=True)
-    ranking = db.Column(db.Integer, nullable=True)
-    review = db.Column(db.String(250), nullable=True)
-    img_url = db.Column(db.String(250), nullable=False)
-db.create_all()
-
-
-## After adding the new_movie the code needs to be commented out/deleted.
-## So you are not trying to add the same movie twice.
-# new_movie = Movie(
-#     title="Phone Booth",
-#     year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-#     rating=7.3,
-#     ranking=10,
-#     review="My favourite character was the caller.",
-#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-# )
-# db.session.add(new_movie)
-# db.session.commit()
-
-
+# all Flask routes below
 @app.route("/")
 def home():
-    all_movies = Movie.query.all()
-    return render_template("index.html", movies=all_movies)
-
-@app.route("/delete")
-def delete_movie():
-    movie_id = request.args.get("id")
-    movie = Movie.query.get(movie_id)
-    db.session.delete(movie)
-    db.session.commit()
-    return redirect(url_for("home"))
-
-class FindMovieForm(FlaskForm):
-    title = StringField("Movie Title", validators=[DataRequired()])
-    submit = SubmitField("Add Movie")
+    return render_template("index.html")
 
 
-@app.route("/add", methods=["GET", "POST"])
-def add_movie():
-    form = FindMovieForm()
-
+@app.route('/add')
+def add_cafe():
+    form = CafeForm()
     if form.validate_on_submit():
-        movie_title = form.title.data
-        response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_DB_API_KEY, "query": movie_title})
-        data = response.json()["results"]
-        return render_template("select.html", options=data)
+        print("True")
+    # Exercise:
+    # Make the form write a new row into cafe-data.csv
+    # with   if form.validate_on_submit()
+    return render_template('add.html', form=form)
 
-    return render_template("add.html", form=form)
 
-@app.route("/find")
-def find_movie():
-    movie_api_id = request.args.get("id")
-    if movie_api_id:
-        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
-        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
-        data = response.json()
-        new_movie = Movie(
-            title=data["title"],
-            year=data["release_date"].split("-")[0],
-            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
-            description=data["overview"]
-        )
-        db.session.add(new_movie)
-        db.session.commit()
-        return redirect(url_for("home"))
+@app.route('/cafes')
+def cafes():
+    with open('cafe-data.csv', newline='') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+    return render_template('cafes.html', cafes=list_of_rows)
 
-@app.route("/")
-def home():
-    all_movies = Movie.query.order_by(Movie.rating).all()
-    for i in range(len(all_movies)):
-        all_movies[i].ranking = len(all_movies) - i
-    db.session.commit()
-    return render_template("index.html", movies=all_movies)
 
 if __name__ == '__main__':
     app.run(debug=True)
