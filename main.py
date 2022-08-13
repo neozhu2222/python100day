76d70ebfb96a6d1a66010f4ba66f4649e1e35fc1
neozhu2222 <109549221+neozhu2222@users.noreply.github.com>
2022-08-13 17:23:16 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,64 +1,118 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 
-print(df_tesla.shape)
-df_tesla.head()
-print(f'Largest value for Tesla in Web Search: {df_tesla.TSLA_WEB_SEARCH.max()}')
-print(f'Smallest value for Tesla in Web Search: {df_tesla.TSLA_WEB_SEARCH.min()}')
-print(df_unenployment.shape)
-df_unemployment.head()
-print('Largest value for "Unenployment Benefits" '
-      f'in Web Search: {df_unemployment.UE_BENEFITS_WEB_SEARCH.max()}')
-print(df_btc_price.head())
-print(df_btc_search.shape)
-df_btc_search.head()
-print(f'largest BTC News Search {df_btc_search.BTC_NEWS_SEARCH.max()}')
-print(f'Missing values for Tesla?: {df_tesla.isna().values.any()}')
-print(f'Missing values for U/E?: {df_unemployment.isna().values.any()}')
-print(f'Missing values for BTC Search?: {df_btc_search.isna().values.any()}')
-print(f'Missing values for BTC Price?: {df_btc_search.isna().values.any()}')
-print(f'Number of missing values: {df_btc_price.isna().values.sum()}')
-df_btc_price[df_btc_price.CLOSE.isna()]
-df_btc_price = df_btc_price.dropna(inplace = True)
-df_tesla.MONTH = pd.to_datetime(df_tesla.MONTH)
-df_btc_search.MONTH = pd.to_datetime(df_btc_search.MONTH)
-df_unemployment.MONTH = pd.to_datetime(df_unemployment.MONTH)
-df_btc_price.DATE = pd.to_datetime(df_btc_price.DATE)
-type(df_tesla.MONTH[0])
-df_tesla.MONTH = pd.to_datetime(df_tesla.MONTH)
-df_btc_search.MONTH = pd.to_datetime(df_btc_search.MONTH)
-df_unenployment.MONTH = pd.to_datetime(df_unenployment.MONTH)
-df_btc_price.DATE = pd.to_datetime(df_btc_price.DATE)
-df_btc_monthly = df_btc_price.resample('M', on='DATE').last()
-plt.figure(figsize=(14,8), dpi=120)
-plt.title('Rolling Monthly US "Unemployment Benefits" Web Searches vs UNRATE', fontsize=18)
-plt.yticks(fontsize=14)
-plt.xticks(fontsize=14, rotation=45)
-ax1 = plt.gca()
-ax2 = ax1.twinx()
-ax1.xaxis.set_major_locator(years)
-ax1.xaxis.set_major_formatter(years_fmt)
-ax1.xaxis.set_minor_locator(months)
-ax1.set_ylabel('FRED U/E Rate', color='purple', fontsize=16)
-ax2.set_ylabel('Search Trend', color='skyblue', fontsize=16)
-ax1.set_ylim(bottom=3, top=10.5)
-ax1.set_xlim([df_unemployment.MONTH[0], df_unemployment.MONTH.max()])
-roll_df = df_unemployment[['UE_BENEFITS_WEB_SEARCH', 'UNRATE']].rolling(window=6).mean()
-ax1.plot(df_unemployment.MONTH, roll_df.UNRATE, 'purple', linewidth=3, linestyle='-.')
-ax2.plot(df_unemployment.MONTH, roll_df.UE_BENEFITS_WEB_SEARCH, 'skyblue', linewidth=3)
-plt.show()
-df_ue_2020 = pd.read_csv('UE Benefits Search vs UE Rate 2004-20.csv')
-df_ue_2020.MONTH = pd.to_datetime(df_ue_2020.MONTH)
-plt.figure(figsize=(14, 8), dpi=120)
-plt.yticks(fontsize=14)
-plt.xticks(fontsize=14, rotation=45)
-plt.title('Monthly US "Unemployment Benefits" Web Search vs UNRATE incl 2020', fontsize=18)
-ax1 = plt.gca()
-ax2 = ax1.twinx()
-ax1.set_ylabel('FRED U/E Rate', color='purple', fontsize=16)
-ax2.set_ylabel('Search Trend', color='skyblue', fontsize=16)
-ax1.set_xlim([df_ue_2020.MONTH.min(), df_ue_2020.MONTH.max()])
-ax1.plot(df_ue_2020.MONTH, df_ue_2020.UNRATE, 'purple', linewidth=3)
-ax2.plot(df_ue_2020.MONTH, df_ue_2020.UE_BENEFITS_WEB_SEARCH, 'skyblue', linewidth=3)
-plt.show()
+
+df_apps_clean = df_apps.dropna()
+df_apps_clean.shape
+duplicated_rows = df_apps_clean[df_apps_clean.duplicated()]
+print(duplicated_rows.shape)
+duplicated_rows.head()
+df_apps_clean = df_apps_clean.drop_duplicates(sebset=['App', 'Type', 'Price'])
+df_apps_clean.sort_values('Rating', ascending=False).head()
+ratings = df_apps_clean.Content_Rating.value_counts()
+fig = px.pie(labels=ratings.index,
+             values=ratings.values,
+             title="Content Rating",
+             names=ratings.index,
+             )
+fig.update_traces(textposition='outside', textinfo='percent+label')
+
+fig.show()
+
+df_apps_clean.Price = df_apps_clean.Price.astype(str).str.replace('$', "")
+df_apps_clean.Price = pd.to_numeric(df_apps_clean.Price)
+
+df_apps_clean.sort_values('Price', ascending=False).head(20)
+df_apps_clean = df_apps_clean[df_apps_clean['Price'] < 250]
+df_apps_clean.sort_values('Price', ascending=False).head(5)
+df_apps_clean['Revenue_Estimate'] = df_apps_clean.Installs.mul(df_apps_clean.Price)
+df_apps_clean.sort_values('Revenue_Estimate', ascending=False)[:10]
+df_apps_clean.Category.nunique()
+top10_category = df_apps_clean.Category.value_counts()[:10]
+bar = px.bar(x=top10_category.index,  # index = category name
+             y=top10_category.values)
+bar.show()
+category_installs = df_apps_clean.groupby('Category').agg({'Installs': pd.Series.sum})
+category_installs.sort_values('Installs', ascending=True, inplace=True)
+h_bar = px.bar(x=category_installs.Installs,
+               y=category_installs.index,
+               orientation='h')
+h_bar.show()
+h_bar = px.bar(x=category_installs.Installs,
+               y=category_installs.index,
+               orientation='h',
+               title='Category Popularity')
+h_bar.update_layout(xaxis_title='Number of Downloads', yaxis_title='Category')
+h_bar.show()
+cat_number = df_apps_clean.groupby('Category').agg({'App': pd.Series.count})
+cat_merged_df = pd.merge(cat_number, category_installs, on='Category', how="inner")
+print(f'The dimensions of the DataFrame are: {cat_merged_df.shape}')
+cat_merged_df.sort_values('Installs', ascending=False)
+scatter = px.scatter(cat_merged_df,
+                     x='App',
+                     y='Installs',
+                     title='Category Concentration',
+                     size='App',
+                     hover_name=cat_merged_df.index,
+                     color='Installs')
+
+scatter.update_layout(xaxis_title="Number of Apps (Lower=More Concentrated)",
+                      yaxis_title="Installs",
+                      yaxis=dict(type='log'))
+scatter.show()
+stack = df_apps_clean.Genres.str.split(';', expand=True).stack()
+print(f'We now have a single column with shape: {stack.shape}')
+num_genres = stack.value_counts()
+print(f'Number of genres: {len(num_genres)}')
+bar = px.bar(x=num_genres.index[:15],
+             y=num_genres.values[:15],
+             title='Top Genres',
+             hover_name=num_genres.index[:15],
+             color=num_genres.values[:15],
+             color_continuous_scale='Agsunset')
+bar.update_layout(xaxis_title='Genre',
+                  yaxis_title='Number of Apps',
+                  coloraxis_showscale=False)
+bar.show()
+df_apps_clean.Type.value_counts()
+df_free_vs_paid = df_apps_clean.groupby(["Category", "Type"], as_index=False).agg({'App': pd.Series.count})
+df_free_vs_paid.head()
+g_bar = px.bar(df_free_vs_paid,
+               x='Category',
+               y='App',
+               title='Free vs Paid Apps by Category',
+               color='Type',
+               barmode='group')
+g_bar.update_layout(xaxis_title='Category',
+                    yaxis_title='Number of Apps',
+                    xaxis={'categoryorder': 'total descending'},
+                    yaxis=dict(type='log'))
+g_bar.show()
+box = px.box(df_apps_clean,
+             y='Installs',
+             x='Type',
+             color='Type',
+             notched=True,
+             points='all',
+             title='How Many Downloads are Paid Apps Giving Up?')
+box.update_layout(yaxis=dict(type='log'))
+box.show()
+df_paid_apps = df_apps_clean[df_apps_clean['Type'] == 'Paid']
+box = px.box(df_paid_apps,
+             x='Category',
+             y='Revenue_Estimate',
+             title='How Much Can Paid Apps Earn?')
+box.update_layout(xaxis_title='Category',
+                  yaxis_title='Paid App Ballpark Revenue',
+                  xaxis={'categoryorder': 'min ascending'},
+                  yaxis=dict(type='log'))
+box.show()
+df_paid_apps.Price.median()
+box = px.box(df_paid_apps,
+             x='Category',
+             y="Price",
+             title='Price per Category')
+box.update_layout(xaxis_title='Category',
+                  yaxis_title='Paid App Price',
+                  xaxis={'categoryorder': 'max descending'},
+                  yaxis=dict(type='log'))
+box.show()
+
