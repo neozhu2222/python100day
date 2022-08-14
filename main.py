a437ba79c97a60064cde4ee614dd74d4b3d40b69
neozhu2222 <109549221+neozhu2222@users.noreply.github.com>
2022-08-14 18:40:36 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,99 +1,16 @@
-data.isna().values.any()
-data.duplicated().values.any()
-duplicated_rows = data[data.duplicated()]
-len(duplicated_rows)
-chars_to_remove = [',', '$']
-columns_to_clean = ['USD_Production_Budget',
-                    'USD_Worldwide_Gross',
-                    'USD_Domestic_Gross']
-for col in columns_to_clean:
-    for char in chars_to_remove:
-        data[col] = data[col].astype(str).str.replace(char, "")
-    data[col] = pd.to_numeric(data[col])
+# This is a sample Python script.
 
-zero_domestic = data[data.USD_Domestic_Gross == 0]
-print(f'Number of films that grossed $0 domestically {len(zero_domestic)}')
-zero_domestic.sort_values('USD_Production_Budget', ascending=False)
-zero_worldwide = data[data.USD_Worldwide_Gross == 0]
-print(f'Number of films that grossed $0 worldwide {len(zero_worldwide)}')
-zero_worldwide.sort_values('USD_Production_Budget', ascending=False)
-international_releases = data.query('USD_Domestic_Gross == 0 and USD_Worldwide_Gross != 0')
-print(f'Number of international releases: {len(international_releases)}')
-international_releases.tail()
-data_clean = data.drop(future_releases.index)
-sns.scatterplot(data=data_clean,
-                x='USD_Production_Budget',
-                y='USD_Worldwide_Gross')
-plt.figure(figsize=(8, 4), dpi=200)
-ax = sns.scatterplot(data=data_clean,
-                     x='USD_Production_Budget',
-                     y='USD_Worldwide_Gross',
-                     hue='USD_Worldwide_Gross',  # colour
-                     size='USD_Worldwide_Gross', )  # dot size
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-ax.set(ylim=(0, 3000000000),
-       xlim=(0, 450000000),
-       ylabel='Revenue in $ billions',
-       xlabel='Budget in $100 millions', )
 
-plt.show()
-plt.figure(figsize=(8,4), dpi=200)
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-# set styling on a single chart
-with sns.axes_style('darkgrid'):
-  ax = sns.scatterplot(data=data_clean,
-                       x='USD_Production_Budget',
-                       y='USD_Worldwide_Gross',
-                       hue='USD_Worldwide_Gross',
-                       size='USD_Worldwide_Gross')
 
-  ax.set(ylim=(0, 3000000000),
-        xlim=(0, 450000000),
-        ylabel='Revenue in $ billions',
-        xlabel='Budget in $100 millions')
-  plt.figure(figsize=(8, 4), dpi=200)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
 
-  with sns.axes_style("darkgrid"):
-      ax = sns.scatterplot(data=data_clean,
-                           x='Release_Date',
-                           y='USD_Production_Budget',
-                           hue='USD_Worldwide_Gross',
-                           size='USD_Worldwide_Gross', )
-
-      ax.set(ylim=(0, 450000000),
-             xlim=(data_clean.Release_Date.min(), data_clean.Release_Date.max()),
-             xlabel='Year',
-             ylabel='Budget in $100 millions')
-dt_index = pd.DatetimeIndex(data_clean.Release_Date)
-years = dt_index.year
-decades = years//10*10
-data_clean['Decade'] = decades
-old_films = data_clean[data_clean.Decade <= 1960]
-new_films = data_clean[data_clean.Decade > 1960]
-sns.regplot(data=old_films,
-            x='USD_Production_Budget',
-            y='USD_Worldwide_Gross')
-plt.figure(figsize=(8,4), dpi=200)
-with sns.axes_style("whitegrid"):
-  sns.regplot(data=old_films,
-            x='USD_Production_Budget',
-            y='USD_Worldwide_Gross',
-            scatter_kws = {'alpha': 0.4},
-            line_kws = {'color': 'black'})
-  plt.figure(figsize=(8,4), dpi=200)
-with sns.axes_style('darkgrid'):
-  ax = sns.regplot(data=new_films,
-                   x='USD_Production_Budget',
-                   y='USD_Worldwide_Gross',
-                   color='#2f4b7c',
-                   scatter_kws = {'alpha': 0.3},
-                   line_kws = {'color': '#ff7c43'})
-
-  ax.set(ylim=(0, 3000000000),
-         xlim=(0, 450000000),
-         ylabel='Revenue in $ billions',
-         xlabel='Budget in $100 millions')
-  budget = 350000000
-  revenue_estimate = regression.intercept_[0] + regression.coef_[0, 0] * budget
-  revenue_estimate = round(revenue_estimate, -6)
-  print(f'The estimated revenue for a $350 film is around ${revenue_estimate:.10}.')
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
