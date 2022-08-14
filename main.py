cebb1acc04d6394e93025c8c055d5596c2282ad9
neozhu2222 <109549221+neozhu2222@users.noreply.github.com>
2022-08-14 15:39:09 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,118 +1,99 @@
+data.isna().values.any()
+data.duplicated().values.any()
+duplicated_rows = data[data.duplicated()]
+len(duplicated_rows)
+chars_to_remove = [',', '$']
+columns_to_clean = ['USD_Production_Budget',
+                    'USD_Worldwide_Gross',
+                    'USD_Domestic_Gross']
+for col in columns_to_clean:
+    for char in chars_to_remove:
+        data[col] = data[col].astype(str).str.replace(char, "")
+    data[col] = pd.to_numeric(data[col])
 
+zero_domestic = data[data.USD_Domestic_Gross == 0]
+print(f'Number of films that grossed $0 domestically {len(zero_domestic)}')
+zero_domestic.sort_values('USD_Production_Budget', ascending=False)
+zero_worldwide = data[data.USD_Worldwide_Gross == 0]
+print(f'Number of films that grossed $0 worldwide {len(zero_worldwide)}')
+zero_worldwide.sort_values('USD_Production_Budget', ascending=False)
+international_releases = data.query('USD_Domestic_Gross == 0 and USD_Worldwide_Gross != 0')
+print(f'Number of international releases: {len(international_releases)}')
+international_releases.tail()
+data_clean = data.drop(future_releases.index)
+sns.scatterplot(data=data_clean,
+                x='USD_Production_Budget',
+                y='USD_Worldwide_Gross')
+plt.figure(figsize=(8, 4), dpi=200)
+ax = sns.scatterplot(data=data_clean,
+                     x='USD_Production_Budget',
+                     y='USD_Worldwide_Gross',
+                     hue='USD_Worldwide_Gross',  # colour
+                     size='USD_Worldwide_Gross', )  # dot size
 
-df_apps_clean = df_apps.dropna()
-df_apps_clean.shape
-duplicated_rows = df_apps_clean[df_apps_clean.duplicated()]
-print(duplicated_rows.shape)
-duplicated_rows.head()
-df_apps_clean = df_apps_clean.drop_duplicates(sebset=['App', 'Type', 'Price'])
-df_apps_clean.sort_values('Rating', ascending=False).head()
-ratings = df_apps_clean.Content_Rating.value_counts()
-fig = px.pie(labels=ratings.index,
-             values=ratings.values,
-             title="Content Rating",
-             names=ratings.index,
-             )
-fig.update_traces(textposition='outside', textinfo='percent+label')
+ax.set(ylim=(0, 3000000000),
+       xlim=(0, 450000000),
+       ylabel='Revenue in $ billions',
+       xlabel='Budget in $100 millions', )
 
-fig.show()
+plt.show()
+plt.figure(figsize=(8,4), dpi=200)
 
-df_apps_clean.Price = df_apps_clean.Price.astype(str).str.replace('$', "")
-df_apps_clean.Price = pd.to_numeric(df_apps_clean.Price)
+# set styling on a single chart
+with sns.axes_style('darkgrid'):
+  ax = sns.scatterplot(data=data_clean,
+                       x='USD_Production_Budget',
+                       y='USD_Worldwide_Gross',
+                       hue='USD_Worldwide_Gross',
+                       size='USD_Worldwide_Gross')
 
-df_apps_clean.sort_values('Price', ascending=False).head(20)
-df_apps_clean = df_apps_clean[df_apps_clean['Price'] < 250]
-df_apps_clean.sort_values('Price', ascending=False).head(5)
-df_apps_clean['Revenue_Estimate'] = df_apps_clean.Installs.mul(df_apps_clean.Price)
-df_apps_clean.sort_values('Revenue_Estimate', ascending=False)[:10]
-df_apps_clean.Category.nunique()
-top10_category = df_apps_clean.Category.value_counts()[:10]
-bar = px.bar(x=top10_category.index,  # index = category name
-             y=top10_category.values)
-bar.show()
-category_installs = df_apps_clean.groupby('Category').agg({'Installs': pd.Series.sum})
-category_installs.sort_values('Installs', ascending=True, inplace=True)
-h_bar = px.bar(x=category_installs.Installs,
-               y=category_installs.index,
-               orientation='h')
-h_bar.show()
-h_bar = px.bar(x=category_installs.Installs,
-               y=category_installs.index,
-               orientation='h',
-               title='Category Popularity')
-h_bar.update_layout(xaxis_title='Number of Downloads', yaxis_title='Category')
-h_bar.show()
-cat_number = df_apps_clean.groupby('Category').agg({'App': pd.Series.count})
-cat_merged_df = pd.merge(cat_number, category_installs, on='Category', how="inner")
-print(f'The dimensions of the DataFrame are: {cat_merged_df.shape}')
-cat_merged_df.sort_values('Installs', ascending=False)
-scatter = px.scatter(cat_merged_df,
-                     x='App',
-                     y='Installs',
-                     title='Category Concentration',
-                     size='App',
-                     hover_name=cat_merged_df.index,
-                     color='Installs')
+  ax.set(ylim=(0, 3000000000),
+        xlim=(0, 450000000),
+        ylabel='Revenue in $ billions',
+        xlabel='Budget in $100 millions')
+  plt.figure(figsize=(8, 4), dpi=200)
 
-scatter.update_layout(xaxis_title="Number of Apps (Lower=More Concentrated)",
-                      yaxis_title="Installs",
-                      yaxis=dict(type='log'))
-scatter.show()
-stack = df_apps_clean.Genres.str.split(';', expand=True).stack()
-print(f'We now have a single column with shape: {stack.shape}')
-num_genres = stack.value_counts()
-print(f'Number of genres: {len(num_genres)}')
-bar = px.bar(x=num_genres.index[:15],
-             y=num_genres.values[:15],
-             title='Top Genres',
-             hover_name=num_genres.index[:15],
-             color=num_genres.values[:15],
-             color_continuous_scale='Agsunset')
-bar.update_layout(xaxis_title='Genre',
-                  yaxis_title='Number of Apps',
-                  coloraxis_showscale=False)
-bar.show()
-df_apps_clean.Type.value_counts()
-df_free_vs_paid = df_apps_clean.groupby(["Category", "Type"], as_index=False).agg({'App': pd.Series.count})
-df_free_vs_paid.head()
-g_bar = px.bar(df_free_vs_paid,
-               x='Category',
-               y='App',
-               title='Free vs Paid Apps by Category',
-               color='Type',
-               barmode='group')
-g_bar.update_layout(xaxis_title='Category',
-                    yaxis_title='Number of Apps',
-                    xaxis={'categoryorder': 'total descending'},
-                    yaxis=dict(type='log'))
-g_bar.show()
-box = px.box(df_apps_clean,
-             y='Installs',
-             x='Type',
-             color='Type',
-             notched=True,
-             points='all',
-             title='How Many Downloads are Paid Apps Giving Up?')
-box.update_layout(yaxis=dict(type='log'))
-box.show()
-df_paid_apps = df_apps_clean[df_apps_clean['Type'] == 'Paid']
-box = px.box(df_paid_apps,
-             x='Category',
-             y='Revenue_Estimate',
-             title='How Much Can Paid Apps Earn?')
-box.update_layout(xaxis_title='Category',
-                  yaxis_title='Paid App Ballpark Revenue',
-                  xaxis={'categoryorder': 'min ascending'},
-                  yaxis=dict(type='log'))
-box.show()
-df_paid_apps.Price.median()
-box = px.box(df_paid_apps,
-             x='Category',
-             y="Price",
-             title='Price per Category')
-box.update_layout(xaxis_title='Category',
-                  yaxis_title='Paid App Price',
-                  xaxis={'categoryorder': 'max descending'},
-                  yaxis=dict(type='log'))
-box.show()
+  with sns.axes_style("darkgrid"):
+      ax = sns.scatterplot(data=data_clean,
+                           x='Release_Date',
+                           y='USD_Production_Budget',
+                           hue='USD_Worldwide_Gross',
+                           size='USD_Worldwide_Gross', )
 
+      ax.set(ylim=(0, 450000000),
+             xlim=(data_clean.Release_Date.min(), data_clean.Release_Date.max()),
+             xlabel='Year',
+             ylabel='Budget in $100 millions')
+dt_index = pd.DatetimeIndex(data_clean.Release_Date)
+years = dt_index.year
+decades = years//10*10
+data_clean['Decade'] = decades
+old_films = data_clean[data_clean.Decade <= 1960]
+new_films = data_clean[data_clean.Decade > 1960]
+sns.regplot(data=old_films,
+            x='USD_Production_Budget',
+            y='USD_Worldwide_Gross')
+plt.figure(figsize=(8,4), dpi=200)
+with sns.axes_style("whitegrid"):
+  sns.regplot(data=old_films,
+            x='USD_Production_Budget',
+            y='USD_Worldwide_Gross',
+            scatter_kws = {'alpha': 0.4},
+            line_kws = {'color': 'black'})
+  plt.figure(figsize=(8,4), dpi=200)
+with sns.axes_style('darkgrid'):
+  ax = sns.regplot(data=new_films,
+                   x='USD_Production_Budget',
+                   y='USD_Worldwide_Gross',
+                   color='#2f4b7c',
+                   scatter_kws = {'alpha': 0.3},
+                   line_kws = {'color': '#ff7c43'})
+
+  ax.set(ylim=(0, 3000000000),
+         xlim=(0, 450000000),
+         ylabel='Revenue in $ billions',
+         xlabel='Budget in $100 millions')
+  budget = 350000000
+  revenue_estimate = regression.intercept_[0] + regression.coef_[0, 0] * budget
+  revenue_estimate = round(revenue_estimate, -6)
+  print(f'The estimated revenue for a $350 film is around ${revenue_estimate:.10}.')
